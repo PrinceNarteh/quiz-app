7f31ff4c039ff2dc7ed3999c474cde26a48e2889
Prince Narteh <prinarteh@gmail.com>
2023-01-06 15:57:58 +0000

    change from stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,54 @@ import "package:flutter/material.dart";
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  List<String> questions = [
+    "What's your favorite color?",
+    "What's your favorite food?",
+    "What's your favorite animal?",
+  ];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-        title: const Text("Quiz"),
-      )),
+        appBar: AppBar(
+          title: const Text("Quiz"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(questions[_questionIndex]),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: const Text("Answer One"),
+            ),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: const Text("Answer Two"),
+            ),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: const Text("Answer Three"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
